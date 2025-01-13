ae17ba06c0a86860a2b2d45ccc2d8e2eb5a075e7
Felipe Basurto <felipe.basurto@ailylabs.com>
2025-01-13 15:19:07 +0100

    Initial Commit

---
+++ kalki/kalkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalkiApp.swift
+//  kalki
+//
+//  Created by Felipe Basurto on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct kalkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
